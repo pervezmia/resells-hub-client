188212dae5a5ca9be41e47da63dce9a0a8012d8c
pervezmia <ahmedpervezkabir@gmail.com>
2026-08-14 10:21:28 +0600

    google providers added

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -9,6 +9,13 @@ const client = new MongoClient(process.env.MONGO_DB_URI);
 const db = client.db("resells-hub");
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL, 
+    socialProviders: {
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID , 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET, 
+        }, 
+    },
   emailAndPassword: {
     enabled: true,
   },
